40a322c63bed233f51c04df5cda7cb98231f0254
Amirhossein Ayoubi <amirhosseinayoubi83@gmail.com>
2023-11-01 03:48:40 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+Console.WriteLine(" enter your number please,we change it to the factoriel ");
+int number =Convert.ToInt32(Console.ReadLine());
+int zarb = 1;
+int a = 0;
+
+for(int i = 0; i < number; i++)
+{
+    zarb = zarb * (a + 1);
+    a++;
+}
+Console.WriteLine(zarb);
+
+
+
+
+
+
+
